4ddc6140c697af53cb047ffb32e8636aa0a71ba9
Brad Mortensen <b.mort86@gmail.com>
2019-06-30 18:17:40 -0600

    adds even emitters

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const app = require('express')()
 const http = require('http').createServer(app)
+const io = require('socket.io')(http);
 
-app.get('/',(req, res)=>res.send('<h1>HelloBoiiiiz</h1>'))
 const port = process.env.PORT || 3000
 
-http.listen(port, ()=>console.log(`listening on port: ${port}`))
+app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'))
+
+io.on('connection', socket => {
+  console.log(`a user connected`)
+  socket.on('disconnect', () => console.log(`user disconnected`))
+  socket.on('chat message', msg => io.emit('chat message', msg))
+})
+
+http.listen(port, () => console.log(`listening on port: ${port}`))
